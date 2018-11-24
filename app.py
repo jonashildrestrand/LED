ee6de485b266393b622fcd0d667ae1da389d463c
Jonas Hildrestrand <jonas@hildrestrand.net>
2018-11-24 15:43:11 +0000

    4x9

--- app.py
+++ app.py
@@ -1,37 +1,57 @@
 from flask import request, url_for
 from flask_api import FlaskAPI, status, exceptions
 import matrix
-from modes import Music
+from modes import Mode, Music
 from flask_cors import CORS
 
 app = FlaskAPI(__name__);
 CORS(app)
 
-board = matrix.Board(9,2);
+board = matrix.Board(9,4);
 
-modes = [
-    None,
-    Music(board)
-];
+state = {
+    "current": 0,
+    "modes": [
+        None,
+        Music(board)
+    ]
+}
+
+def stateToJson():
+    json = {
+        'current': state['current'],
+        'modes': []
+    }
+
+    for mode in state['modes']:
+        if(mode is None):
+            json['modes'].append(None)
+        elif(isinstance(mode, Mode)):
+            json['modes'].append(mode.config)
+    return json
 
 @app.route("/get/modes", methods=['GET'])
-def getMode():
-    return modes, 200
+def getModes():
+    return stateToJson(), 200
 
 @app.route("/get/mode", methods=['GET'])
 def getMode():
     if board.getMode() is not None:
-        return board.getMode().config, 200
+        return board.getMode(), 200
     else:
         return {}, 404
 
 @app.route("/set/mode", methods=['POST'])
 def toggleMusic():
-    mode = request.form.get('mode')
-    if mode in modes:
-        board.setMode(modes[mode])
-        return {}, 200
-    return {}, 404
+    print(request.data.get('mode'))
+    mode = request.data.get('mode')
+
+    try:
+        state['current'] = mode
+        board.setMode(state['modes'][mode])
+        return stateToJson(), 200
+    except KeyError:
+        return {}, 404
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
